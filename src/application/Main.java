b0233fc7e29e2c042b567d3d42bb01db344f5f77
Batgerelt <32481226+batgrerelt@users.noreply.github.com>
2019-05-16 10:55:56 +0800

    inisial commit
    Шүдний эмнэлэг суурь

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("MainWindowFxml.fxml"));
+			
+			primaryStage.getIcons().add(new Image("file:icon.png"));
+			primaryStage.setTitle("Dentist Medical");
+			primaryStage.setScene(new Scene(root, 1160, 850));
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/MainWindowController.java
@@ -0,0 +1,114 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainWindowController {
+
+    @FXML
+    private BorderPane rootPane;
+
+    @FXML
+    private ChoiceBox<?> choice_udurharah;
+
+    @FXML
+    private Button button_umnuhdoloohonog;
+
+    @FXML
+    private Button button_enedoloohonog;
+
+    @FXML
+    private Button button_daraadoloohonog;
+
+    @FXML
+    private Label time_lbl1;
+
+    @FXML
+    private Label time_lbl2;
+
+    @FXML
+    private Label time_lbl3;
+
+    @FXML
+    private Label time_lbl4;
+
+    @FXML
+    private Label time_lbl5;
+
+    @FXML
+    private Label time_lbl6;
+
+    @FXML
+    private Label time_lbl7;
+
+    @FXML
+    private Label time_lbl8;
+
+    @FXML
+    private Label time_lbl9;
+
+    @FXML
+    private Label day_lbl1;
+
+    @FXML
+    private Label day_lbl2;
+
+    @FXML
+    private Label day_lbl3;
+
+    @FXML
+    private Label day_lbl4;
+
+    @FXML
+    private Label day_lbl5;
+
+    @FXML
+    private DatePicker datepicker_udursongoh;
+
+    @FXML
+    private ChoiceBox<?> choice_uvchtunsongoh;
+
+    @FXML
+    private TextArea textarea1_uvchtunharah;
+
+    @FXML
+    private TextArea textarea_tsaghuviarlalt;
+
+    @FXML
+    private Button button_done;
+
+    @FXML
+    private Button button_cancel;
+
+    @FXML
+    private ToggleGroup face;
+
+    @FXML
+    private ToggleGroup mouth;
+
+    @FXML
+    private ToggleGroup length;
+
+    @FXML
+    private ToggleGroup growth;
+
+    @FXML
+    private ToggleGroup teeth;
+
+    @FXML
+    private Button closeBtn;
+
+    @FXML
+	void handleClickCloseBtn(ActionEvent event) {
+		Stage stage = (Stage) closeBtn.getScene().getWindow();
+		stage.close();
+	}
+}
