fed0467851cb9657ba80e2aea3a49a9f9ac44a1b
Batgerelt <32481226+batgrerelt@users.noreply.github.com>
2019-05-18 21:01:17 +0800

    Design дээх жаахан юм хийсэн

--- src/application/MainWindowController.java
+++ src/application/MainWindowController.java
@@ -1,17 +1,24 @@
 package application;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -109,13 +116,32 @@ public class MainWindowController implements Initializable {
 
     @FXML
     private Button closeBtn;
+    
+    @FXML
+    private ImageView logout;
 
     @FXML
 	void handleClickCloseBtn(ActionEvent event) {
-    	Stage stage = (Stage) closeBtn.getScene().getWindow();
-		stage.close();		
+    	System.out.println("logout");
 	}
     
+    @FXML
+    void handleLogout(MouseEvent  event) {
+    	ButtonType yes = new ButtonType("Тийм", ButtonData.YES);
+    	ButtonType no = new ButtonType("Буцах", ButtonData.CANCEL_CLOSE);
+    	
+    	Alert alert = new Alert(AlertType.WARNING, "",  yes, no); 
+    	alert.setTitle("");
+    	alert.setHeaderText("Та програмаас гарахдаа итгэлтэй байна уу?");
+    	
+    	Optional<ButtonType> result = alert.showAndWait();
+
+    	if (result.orElse(no) == yes) {
+    	    //do stuff
+    		Stage stage = (Stage) logout.getScene().getWindow();
+    		stage.close();
+    	}    	
+    }
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
